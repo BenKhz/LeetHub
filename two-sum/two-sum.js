d333821ee9fc56cd6c5c14ae0d1b2a42e595eab5
Ben Bernardy <benbernardy@gmail.com>
2021-09-07 22:34:37 -0700

    Time: 100 ms (57.33%), Space: 39.3 MB (86.85%) - LeetHub

---
+++ two-sum/two-sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for(var i=0; i<nums.length; i++){
+        for(var j=i+1; j<nums.length; j++){
+            if(target-nums[i] === nums[j]){
+                return [i,j]
+            }
+        }
+        
+    }
+};
