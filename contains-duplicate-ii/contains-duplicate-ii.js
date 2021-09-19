dfa1cadbb8b15187152641f6b62b8bc7e78772e8
Ben Bernardy <benbernardy@gmail.com>
2021-09-18 17:51:57 -0700

    Time: 3836 ms (11.06%), Space: 48 MB (86.64%) - LeetHub

---
+++ contains-duplicate-ii/contains-duplicate-ii.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    for ( var i = 0; i<nums.length; i++) {
+       for (var j = i+1; j<nums.length; j++){
+           if(nums[i] === nums[j] && j-i <= k) {
+               return true;
+           }
+       }
+    }
+    return false;
+};
