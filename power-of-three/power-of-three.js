99cbda499e84f6f1f878e4cb4521855bbdd5ecc9
Ben Bernardy <benbernardy@gmail.com>
2021-10-01 14:13:32 -0700

    Added Base3 Regex string comparison approach

--- power-of-three/power-of-three.js
+++ power-of-three/power-of-three.js
@@ -14,3 +14,10 @@ var isPowerOfThree = function(n) {
         return isPowerOfThree(n)
     }
 };
+
+// Refactored from recursive to use base 3 and regex comparison! Fun stuff!
+
+var isPowerOfThree = function(n) {
+    if(n === 1) { return true }
+    return n.toString(3).search(/1[0]+$/) === 0 ? true : false;
+};
