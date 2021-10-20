6ebcfb530078aa92ecfbd6edf114a23249fe6163
Ben Bernardy <benbernardy@gmail.com>
2021-10-19 20:55:52 -0700

    Added alternative second approach

--- remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.js
+++ remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.js
@@ -3,12 +3,23 @@
  * @return {string}
  */
 var removeDuplicates = function(s) {
-    var result = s.split('').reduce((letters, letter) => {
-        if(letters.slice(-1).join('') === letter) {
-            letters.splice(-1)
-        } else {
-            letters.push(letter) 
-        } return letters;
-    }, [])
-    return result.join('')
+    const stack = [];
+    
+    for(let i = 0; i < s.length; i++) {
+        if(stack[stack.length - 1] !== s[i]) stack.push(s[i])
+        else stack.pop()
+    }
+    return stack.join('')
+    
+    // alternative using splice and slice vs pop and last index by .length.
+    // Below method tested faster than above. ... Leet code is weird.
+    
+//     var result = s.split('').reduce((letters, letter) => {
+//         if(letters.slice(-1).join('') === letter) {
+//             letters.splice(-1)
+//         } else {
+//             letters.push(letter) 
+//         } return letters;
+//     }, [])
+//     return result.join('')
 };
