7bc55f552faae2803e76cd463c2adbcba5ada27c
Ben Bernardy <benbernardy@gmail.com>
2022-02-14 19:01:12 -0800

    Time: 98 ms (48.77%), Space: 44.3 MB (20.78%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.js
+++ 414-third-maximum-number/414-third-maximum-number.js
@@ -3,11 +3,25 @@
  * @return {number}
  */
 var thirdMax = function(nums) {
-    const sortedNums = nums.sort((a,b)=>b-a);
-    var results = [];
-    for(var i=0; i<sortedNums.length; i++) {
-        if(!results.includes(sortedNums[i])) { results.push(nums[i]) }
-        if(results.length === 3) { return results[2] }
+    //O(n) attempt
+    var [first, second, third] = [-Infinity, -Infinity, -Infinity];
+    for(var num of nums) {
+        if(num === first || num === second || num === third) { continue; }
+        if(num > first) {
+            [third, second, first] = [second, first, num];
+        } else if ( num > second ) {
+            [third, second] = [second, num];
+        } else if ( num > third) {
+            third = num;
+        }
     }
-    return results[0];
+     return third === -Infinity ? first : third;
+    // Naive solution
+    // const sortedNums = nums.sort((a,b)=>b-a);
+    // var results = [];
+    // for(var i=0; i<sortedNums.length; i++) {
+    //     if(!results.includes(sortedNums[i])) { results.push(nums[i]) }
+    //     if(results.length === 3) { return results[2] }
+    // }
+    // return results[0];
 };
