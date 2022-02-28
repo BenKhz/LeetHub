fd73bb30afe372c6a34f8e4f72485024f4a33b33
Ben Bernardy <benbernardy@gmail.com>
2022-02-27 21:49:29 -0800

    Time: 92 ms (80.57%), Space: 45.1 MB (12.47%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.js
+++ 905-sort-array-by-parity/905-sort-array-by-parity.js
@@ -3,13 +3,9 @@
  * @return {number[]}
  */
 var sortArrayByParity = function(nums) {
-    var result = nums.reduce((acc, num) => {
-        if(num % 2 === 0) { 
-            acc[0].push(num);
-        } else { 
-            acc[1].push(num);
-        }
+    return nums.reduce((acc, num) => {
+        if(num % 2 === 0) { acc[0].push(num); } 
+        else { acc[1].push(num); }
         return acc;
     }, [[], []] ).flat();
-    return result;
 };
